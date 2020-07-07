00034febe508d1286dfaab2fb76b8ec45337c05c
keyrillanskiy <keyrillanskiy@gmail.com>
2020-07-07 14:42:35 +0300

    Added required set of libraries, added main activity with test code

---
+++ app/src/main/java/com/github/keyrillanskiy/radioactive/presentation/screens/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.github.keyrillanskiy.radioactive.presentation.screens.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.keyrillanskiy.radioactive.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
