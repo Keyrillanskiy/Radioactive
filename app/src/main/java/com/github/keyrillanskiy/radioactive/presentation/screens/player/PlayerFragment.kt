430f1bae50086086f6fe9962c81ecdabacd8c470
keyrillanskiy <keyrillanskiy@gmail.com>
2020-07-12 17:50:03 +0300

    Implemented sample player screen

--- app/src/main/java/com/github/keyrillanskiy/radioactive/presentation/screens/player/PlayerFragment.kt
+++ app/src/main/java/com/github/keyrillanskiy/radioactive/presentation/screens/player/PlayerFragment.kt
@@ -1,13 +1,22 @@
 package com.github.keyrillanskiy.radioactive.presentation.screens.player
 
 import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.github.keyrillanskiy.radioactive.R
 import com.github.keyrillanskiy.radioactive.databinding.FragmentPlayerBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import dagger.android.support.AndroidSupportInjection
+import kotlinx.android.synthetic.main.fragment_player.*
 import ru.terrakok.cicerone.Router
 import javax.inject.Inject
 
@@ -18,6 +27,8 @@ class PlayerFragment : Fragment() {
 
     @Inject
     lateinit var router: Router
+
+    lateinit var exoPlayer: ExoPlayer
 
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
@@ -30,6 +41,30 @@ class PlayerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return FragmentPlayerBinding.inflate(inflater, container, false).root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //test code:
+        val context = requireContext()
+        val appName = getString(R.string.app_name)
+        val dataSourceFactory = DefaultDataSourceFactory(context, Util.getUserAgent(context, appName))
+        val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory, DefaultExtractorsFactory())
+            .createMediaSource(Uri.parse("http://sintezfm.radioactivelab.pro:8000/sfm192.aac"))
+
+        exoPlayer = SimpleExoPlayer.Builder(context).build()
+        exoPlayer.prepare(mediaSource)
+
+        playerPlayPauseButton.setOnClickListener {
+            if (exoPlayer.playWhenReady) {
+                playerPlayPauseButton.setBackgroundResource(R.drawable.ic_play)
+                exoPlayer.playWhenReady = false
+            } else {
+                playerPlayPauseButton.setBackgroundResource(R.drawable.ic_pause)
+                exoPlayer.playWhenReady = true
+            }
+        }
     }
 
 }
